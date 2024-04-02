309f07911a412d46f04db2dbd11a2f80dd8fc5e7
Darek <dareczek06@gmail.com>
2024-04-02 10:26:04 +0200

    Added easter egg
    resolved #1

--- page-project/assets/js/main.js
+++ page-project/assets/js/main.js
@@ -1,1 +1,13 @@
 import '../scss/styles.scss'
+
+const title = document.getElementById("description");
+const catIcon = document.getElementById("cat-icon");
+
+catIcon.addEventListener("click", () => {
+
+  title.textContent = "This website is purrfect!";
+
+  setTimeout(() => {
+    title.textContent = "Coded by Darek Duda";
+  }, 1000);
+});
